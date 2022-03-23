962dabcb2ce114ce94710827fa347a97dd45f89d
youngsjungg <youngsjungg@gmail.com>
2022-03-24 01:30:52 +0900

    Build 첫번째 Todo 시작

--- app/src/main/java/com/example/todo_application/MainActivity.kt
+++ app/src/main/java/com/example/todo_application/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.todo_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo_application.databinding.ActivityMainBinding
+import com.example.todo_application.one.MainActivity2
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTodo1.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+        }
     }
 }
