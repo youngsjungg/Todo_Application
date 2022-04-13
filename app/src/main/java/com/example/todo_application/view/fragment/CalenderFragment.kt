6825fabbf34b3d879642a7b41728fa13d0b72397
youngsjungg <youngsjungg@gmail.com>
2022-04-13 23:21:56 +0900

    <Build> : 캘린더페이지 기능 추가

--- app/src/main/java/com/example/todo_application/view/fragment/CalenderFragment.kt
+++ app/src/main/java/com/example/todo_application/view/fragment/CalenderFragment.kt
@@ -4,15 +4,78 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todo_application.adapter.TodoAdapter
+import com.example.todo_application.data.Memo
+import com.example.todo_application.databinding.FragmentCalenderBinding
+import com.example.todo_application.view.dialog.CustomDialog
+import com.example.todo_application.view.dialog.CustomDialogInterface
+import com.example.todo_application.viewmodel.MemoViewModel
 
-class CalenderFragment : Fragment() {
+class CalenderFragment : Fragment(), CustomDialogInterface {
+
+    private var binding : FragmentCalenderBinding? = null
+    //viewmodel 연결
+    private val memoViewModel : MemoViewModel by viewModels()
+    //Adapter 선언
+    private val adapter : TodoAdapter by lazy { TodoAdapter() }
+
+    private var year = 0
+    private var month = 0
+    private var day = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentCalenderBinding.inflate(inflater,container,false)
+
+        binding!!.recyclerCal.layoutManager = LinearLayoutManager(activity , LinearLayoutManager.VERTICAL, false)
+        binding!!.recyclerCal.adapter = adapter
+
+        binding!!.calendar.setOnDateChangeListener { calendarView, year, month, day ->
+           //날짜 선택시 그 날의 정보할당
+            this.year = year
+            this.month = month+1
+            this.day = day
+
+            binding!!.tvSelectDate.text = "${this.year}/${this.month}/${this.day}"
+
+            //리스트를 관찬하여 변경되면 어댑터에 전달
+            memoViewModel.readDateData(this.year, this.month, this.day).observe(
+                viewLifecycleOwner, Observer {
+                    adapter.setData(it)
+                }
+            )
+        }
+
+
+
+
+        return binding!!.root
+    }
+    private fun onFabClicked() {
+
+        val customDialog = CustomDialog(requireActivity() , this)
+        customDialog.show()
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
+
+    override fun onOkButtonClicked(content: String) {
+        //선택된 날짜로 메모 추가
+        val memo = Memo(false, content, year, month , day)
+        memoViewModel.addMemo(memo)
+        Toast.makeText(activity, "추가", Toast.LENGTH_SHORT).show()
+
+
     }
 }
